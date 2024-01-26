5cad4248117c7883d5f495c50caa67120e3bd8dc
Abd'al-Malik Bourassi <malik.chan94420@gmail.com>
2024-01-26 11:33:53 +0100

    update routing in main.jsx and creation of all the pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./main.css";
+import Home from "./pages/Home/homeKasa.jsx";
+import About from "./pages/About/aboutKasa.jsx";
+import Housing from "./pages/Housing/housingKasa.jsx";
+import Error from "./pages/Error/errorKasa.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/housing" element={<Housing />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
---
+++ src/pages/Housing/housingKasa.jsx
@@ -0,0 +1,3 @@
+export default function Housing() {
+  return <h1>Housing</h1>;
+}
